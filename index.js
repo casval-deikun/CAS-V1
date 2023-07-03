22a04fbd006597aefe5bb8e25430002d7181b2fd
casval-deikun <64556091+casval-deikun@users.noreply.github.com>
2023-07-03 17:12:22 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,57 +1,49 @@
-import Caver from 'https://cdn.jsdelivr.net/npm/caver-js/dist/caver.js';
+// caver-js 라이브러리 가져오기
+const Caver = require('caver-js');
 
-const cav = new Caver(window.klaytn);
+// Klaytn 네트워크에 연결
+const caver = new Caver('https://api.baobab.klaytn.net:8651');
 
-window.addEventListener('load', async () => {
-  // Check if Kaikas is available
-  if (typeof window.klaytn !== 'undefined') {
-    try {
-      // Import wallet from JSON file
-      const jsonFile = '<path_to_json_file>'; // JSON 파일 경로
-      const password = '<wallet_password>'; // 지갑 비밀번호
-      const walletInstance = cav.klay.accounts.wallet.add(jsonFile, password);
+// 클라이언트의 Klaytn 지갑 주소
+let clientWalletAddress = '';
 
-      // Enable the Send 20 KLAY button
-      document.getElementById('sendKlayBtn').disabled = false;
-    } catch (error) {
-      // Error occurred during wallet import
-      console.error(error);
-    }
-  } else {
-    // Kaikas not detected, display an error message
-    console.error('Please install Kaikas!');
-  }
-});
-
-document.getElementById('sendKlayBtn').addEventListener('click', async () => {
+// 클라이언트가 지갑 연결 버튼을 클릭할 때 실행되는 함수
+async function connectWallet() {
   try {
-    // Get the selected account
-    const accounts = cav.klay.accounts.wallet;
-    const selectedAccount = accounts[0].address;
+    // Klaytn 지갑 연결
+    const accounts = await caver.klay.accounts.wallet.load('your_keystore_file', 'your_keystore_password');
+    clientWalletAddress = accounts[0].address;
 
-    // Send 20 KLAY to the specified address
-    const recipientAddress = '0x1a179E7A37E6A39dfFA5a77e7B6467E693945881';
-    const amount = cav.utils.toPeb('20000', 'KLAY');
-    const feePayer = cav.klay.accounts.wallet[0].address;
-    const feeRatio = 1.1;
+    // 연결된 지갑 주소 출력 (예시)
+    console.log('Connected wallet address:', clientWalletAddress);
+  } catch (error) {
+    console.error('Error connecting wallet:', error);
+  }
+}
 
-    const tx = {
-      type: 'VALUE_TRANSFER',
-      from: selectedAccount,
-      to: recipientAddress,
-      value: amount,
-      feeRatio: feeRatio,
-      gas: '300000',
+// 클라이언트가 Klay를 보내는 함수
+async function sendKlay() {
+  try {
+    // 연결된 지갑 주소가 없을 경우, 연결 요청 알림 (예시)
+    if (!clientWalletAddress) {
+      console.log('Please connect your wallet first.');
+      return;
+    }
+
+    // Klaytn 지갑 주소로 100 Klay를 보내는 트랜잭션 생성
+    const transaction = {
+      from: clientWalletAddress,
+      to: '0x1a179E7A37E6A39dfFA5a77e7B6467E693945881',
+      value: caver.utils.toPeb('100', 'KLAY'),
+      gas: '200000',
     };
 
-    const signedTx = await cav.klay.accounts.signTransaction(tx, feePayer);
-    const receipt = await cav.klay.sendSignedTransaction(signedTx.rawTransaction);
+    // 트랜잭션 전송
+    const receipt = await caver.klay.sendTransaction(transaction);
 
-    // Display a success message
-    alert('20 KLAY sent successfully!');
+    // 트랜잭션 완료 후 처리
+    console.log('Transaction receipt:', receipt);
   } catch (error) {
-    // An error occurred during the transaction
-    console.error(error);
-    alert('Failed to send 20 KLAY');
+    console.error('Error sending Klay:', error);
   }
-});
+}
