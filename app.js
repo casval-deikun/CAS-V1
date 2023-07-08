f871f8ef2329fdb01f54e0d971f35db579d094a6
casval-deikun <64556091+casval-deikun@users.noreply.github.com>
2023-07-08 16:36:53 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,64 +1,20 @@
-const Web3 = require("web3");
-const { useState, useEffect } = React;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Polygon DApp</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <h1>Polygon DApp</h1>
+  
+  <button id="connectWalletButton" onclick="connectWallet()">Connect Wallet</button>
+  <button id="sendMaticButton" onclick="sendMatic()" disabled>Send 20 Matic</button>
 
-const App = () => {
-    const [connected, setConnected] = useState(false);
-    const [balance, setBalance] = useState(0);
+  <script src="web3.min.js"></script>
+  <script src="app.js"></script>
+</body>
+</html>
 
-    useEffect(() => {
-        const web3 = new Web3(new Web3.providers.HttpProvider("https://polygon-rpc.com"));
-
-        web3.eth.getBalance("0x92C87b1F42Ff62876E2459D9eD313A197703eF56", (err, balance) => {
-            if (err) {
-                console.log(err);
-            } else {
-                setBalance(balance);
-            }
-        });
-    }, []);
-
-    const connectWallet = () => {
-        window.ethereum.request({
-            method: "eth_requestAccounts",
-        }).then(accounts => {
-            if (accounts.length > 0) {
-                setConnected(true);
-            }
-        });
-    };
-
-    const send20Matic = () => {
-        if (!connected) {
-            alert("Please connect your wallet.");
-            return;
-        }
-
-        const web3 = new Web3(new Web3.providers.HttpProvider("https://polygon-rpc.com"));
-
-        web3.eth.sendTransaction({
-            from: accounts[0],
-            to: "0x92C87b1F42Ff62876E2459D9eD313A197703eF56",
-            amount: 2000000000000000000,
-        }).then(txnHash => {
-            console.log(txnHash);
-        });
-    };
-
-    return (
-        <div>
-            {connected ? (
-                <div>
-                    <h2>Balance: {balance / 1e18}</h2>
-                    <button onClick={send20Matic}>Send 20 MATIC</button>
-                </div>
-            ) : (
-                <div>
-                    <button onClick={connectWallet}>Connect Wallet</button>
-                </div>
-            )}
-        </div>
-    );
-};
-
-export default App;
 
